87f043df562eb723d120db91f6d0ac678d38beba
Bas Vodde <basv@odd-e.com>
2012-09-08 19:46:51 +0800

    Makefile from issue37

--- Makefile
+++ Makefile
@@ -4,6 +4,8 @@ CPPUTEST_INCLUDE = -I $(CPPUTEST_HOME)/include
 CPPUTEST_LIB = $(CPPUTEST_HOME)/lib/libCppUTest.a
 
 CXXFLAGS += $(CPPUTEST_INCLUDE)
+CXXFLAGS += -include $(CPPUTEST_HOME)/include/CppUTest/MemoryLeakDetectorMallocMacros.h
+
 
 all: dummy
 	./dummy
--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
 
 #include "CppUTest/TestHarness.h"
 #include "CppUtest/CommandLineTestRunner.h"
+#include "CppUtest/TestRegistry.h"
+
+
+class MyPlugin: public TestPlugin {
+    int* dummy_;
+public:
+    MyPlugin() :
+            TestPlugin("MyPlugin")
+    {
+        dummy_ = (int*) malloc(100*sizeof(int));
+    }
+    virtual ~MyPlugin()
+    {
+        free(dummy_);
+    }
+
+};
 
 int main(int argc, char** argv)
 {
+	MyPlugin m;
+	TestRegistry::getCurrentRegistry()->installPlugin(&m);
 
 	return CommandLineTestRunner::RunAllTests(argc, argv);
 }
