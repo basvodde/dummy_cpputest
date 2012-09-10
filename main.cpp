98db991d9e78088dc99170016a0b869ccdd26c43
Bas Vodde <basv@www.odd-e.com>
2012-09-09 22:54:06 -0400

    Fixed main for master

--- main.cpp
+++ main.cpp
@@ -3,26 +3,7 @@
 #include "CppUTest/CommandLineTestRunner.h"
 #include "CppUTest/TestRegistry.h"
 
-
-class MyPlugin: public TestPlugin {
-    int* dummy_;
-public:
-    MyPlugin() :
-            TestPlugin("MyPlugin")
-    {
-        dummy_ = (int*) malloc(100*sizeof(int));
-    }
-    virtual ~MyPlugin()
-    {
-        free(dummy_);
-    }
-
-};
-
 int main(int argc, char** argv)
 {
-	MyPlugin m;
-	TestRegistry::getCurrentRegistry()->installPlugin(&m);
-
 	return CommandLineTestRunner::RunAllTests(argc, argv);
 }
